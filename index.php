944a339e13a479ed5528397609acb17ea8e22e1b
aqsa <aqsaaptech73@gmail.com>
2026-06-13 09:25:59 +0500

    ok

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad. Eligendi dolorem cupiditate delectus quisquam enim, facilis sapiente explicabo accusantium vitae est molestiae quasi sit porro. Voluptas pariatur enim accusamus, velit nesciunt corrupti minus deleniti laborum, vitae inventore blanditiis earum, ducimus laboriosam unde labore numquam iure error distinctio suscipit. Sunt.</p>
+</body>
+</html>
